0fa10e5ceb068f6b25fa7015e41aaca7f6c096a9
luis loya <jl.loya@gmail.com>
2014-06-06 01:24:57 -0500

    Trimmed botas.sh

--- botas.sh
+++ botas.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-DEFAULTS="$PWD/defaults/"
+DEFAULTS="$PWD/config/"
 DOTFILES="$PWD/dotfiles/"
-RESOURCES="$PWD/resources/"
 
 GREEN="\033[01;32m"
 YELLOW="\033[0;33m"
@@ -13,21 +12,17 @@ info() {
   printf "$GREEN-----> $NOCOLOR %b\n" "$1"
 }
 
-success() {
-  printf "$GREEN-----> $YELLOW %b$NOCOLOR\n" "$1"
-}
-
 warning() {
   printf "$PINK-----> %b$NOCOLOR\n" "$1"
 }
 
 function installcask() {
-cool "${@}"
+info "${@}"
 brew cask install "${@}" 2> /dev/null
 }
 
 function installbrew() {
-cool "${@}"
+info "${@}"
 brew install "${@}" 2> /dev/null
 }
 
@@ -39,6 +34,7 @@ section(){
 }
 
 info "Escribe el password para sudo"
+
 sudo -v
 
 #-----------------------------------------------
@@ -53,44 +49,36 @@ section "Instalando DOTFILES"
 
 #-----------------------------------------------
 section "HOMEBREW TOLLS & APPS"
-   cout "Instalando homebrew"
+   info "Instalando homebrew"
    if ! which brew &>/dev/null; then
        ruby <(curl -fsS https://raw.github.com/Homebrew/homebrew/go/install)
        brew update
 
      if ! grep -qs "recommended by brew doctor" ~/.bash_profile; then
-       echo "Put Homebrew location earlier in PATH ..."
        echo " " >> ~/.bash_profile
-         echo "# recommended by brew doctor" >> ~/.bash_profile
-         echo "export PATH=\"/usr/local/bin:\$PATH\"\n" >> ~/.bash_profile
-         export PATH="/usr/local/bin:$PATH"
+       echo "# recommended by brew doctor" >> ~/.bash_profile
+       echo "export PATH=\"/usr/local/bin:\$PATH\"\n" >> ~/.bash_profile
+       export PATH="/usr/local/bin:$PATH"
      fi
    else
      warning "Homebrew already installed. Skipping ..."
    fi
 
-   cout "Instalando homebrew-cask"
+   info "Instalando homebrew-cask"
    brew tap phinze/homebrew-cask 2>/dev/null
    brew install brew-cask 2>/dev/null
 
-   section "COMMAND LINE TOOLS"
-   cout "Instalando Command Line Tools"
-   while read -r line; do installbrew $line; done < ./defaults/brew.tools
+   info "Instalando Command Line Tools"
+   while read -r line; do installbrew $line; done < ./config/tools
 
 
-   section "MAC OS X APPS"
-   cout "Instalando OS X Apps"
-   while read -r line; do installcask $line; done < ./defaults/brew.apps
-
-
-   cout "Configurando Tools & Apps"
-   source $DEFAULTS/brew.post
-
+   info "Instalando OS X Apps"
+   while read -r line; do installcask $line; done < ./config/apps
 
 #-----------------------------------------------
 
 #-----------------------------------------------
-section "MAC OS X CONFIG"
+section "MAC OS X CONFIGURATION"
    source $DEFAULTS/osxconfig
 
 #-----------------------------------------------
@@ -102,7 +90,7 @@ section "RVM & RUBY"
    echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile
    source ~/.bash_profile
 
-   while read -r line; do rvm install $line; done < ./defaults/rubies
+   while read -r line; do rvm install $line; done < ./config/rubies
 
 #-----------------------------------------------
 
